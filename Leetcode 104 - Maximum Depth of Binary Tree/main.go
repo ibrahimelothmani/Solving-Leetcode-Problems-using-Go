b6dda88ad8e5b26cc6e950201b052cd76540462c
Ibrahim <ibrahimelothmani98@gmail.com>
2025-06-29 21:05:22 -1200

    Add initial implementation of maxDepth function and TreeNode struct in Go

---
+++ Leetcode 104 - Maximum Depth of Binary Tree/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	root := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 4,
+			},
+			Right: &TreeNode{
+				Val: 5,
+			},
+		},
+		Right: &TreeNode{
+			Val: 3,
+			Right: &TreeNode{
+				Val: 6,
+			},
+		},
+	}
+
+	fmt.Println(maxDepth(root)) // Output: 4
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftDepth := maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+	if leftDepth > rightDepth {
+		return leftDepth + 1
+	}
+	return rightDepth + 1
+}
